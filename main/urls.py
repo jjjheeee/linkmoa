84dabfccddd61e3b413a23f8eb9031424538ba23
wogml8524 <wogml8524@naver.com>
2025-02-13 13:35:09 +0900

    healthcheck

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, healthcheck
 
 urlpatterns = [
+    path('healthcheck', healthcheck, name='healthcheck'),
     path('', index, name='main'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def healthcheck(request):
+
+    return "OK"
+
 def index(request):
     return render(request, "main.html")
