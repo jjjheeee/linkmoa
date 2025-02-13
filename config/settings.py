1cb9396361ad657ae1e04d009b17db84d65916ae
wogml8524 <wogml8524@naver.com>
2025-02-13 15:26:04 +0900

    test

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_CACHE_ALIAS = 'default'
