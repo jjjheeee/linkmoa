5fad351091b9aea74d041a377f08c9c5d08d8835
wogml8524 <wogml8524@naver.com>
2025-03-17 17:29:13 +0900

    sqllite 분리 test

--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -83,7 +85,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 ENVIRONMENT = os.getenv("DJANGO_ENV", "dev")
-
+print(ENVIRONMENT)
 if ENVIRONMENT == "production":
     DATABASES = {
         "default": {
